193cff78598bb037930df5e65c7f7169738db569
minodisk <daisuke.mino@gmail.com>
2013-02-07 17:39:48 +0900

    Compile 3 versions raw, min, and normal.

--- make.js
+++ make.js
@@ -20,8 +20,10 @@
     , DST = 'lib'
     , NAME = 'math3'
 
+    , sys = require('sys')
     , fs = require('fs')
     , path = require('path')
+    , uglify = require('uglify-js')
 
     , changed = (function () {
       var id
@@ -39,26 +41,29 @@
         , code
         ;
       FILES.forEach(function (filename) {
-        var lines = fs.readFileSync(path.join(SRC, filename), 'utf8')
-            .split(/\r?\n/)
-          ;
-        lines.forEach(function (line, i) {
-          lines[i] = '  ' + line;
-        });
-        codes.push(lines.join('\n'));
+        codes.push(fs.readFileSync(path.join(SRC, filename), 'utf8'));
       });
       EXPORTS.forEach(function (exp) {
-        exports.push('  this.' + exp + ' = ' + exp + ';');
+        exports.push('this.' + exp + ' = ' + exp + ';');
       });
       codes.push(exports.join('\n'));
 
+      code = codes.join('\n\n\n');
+      fs.writeFileSync(path.join(DST, NAME + '.raw.js'), code);
+
       code = [
         '(function () {',
-        codes.join('\n\n\n'),
+        code,
         '}).call(this);'
       ].join('\n');
-
       fs.writeFileSync(path.join(DST, NAME + '.js'), code);
+
+      code = uglify.minify(code, {
+        fromString: true
+      });
+      fs.writeFileSync(path.join(DST, NAME + '.min.js'), code);
+
+      sys.puts([Date.now(), ':', 'Compiled'].join(' '));
     }
     ;
 
